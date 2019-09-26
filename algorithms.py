f1326e5e939575ca9d793bb7287137ab100dd94a
Ryan Cirincione <ryancirincione@aol.com>
2019-09-26 17:34:56 -0400

    Documented search and fixed it

--- algorithms.py
+++ algorithms.py
@@ -1,30 +1,35 @@
 import numpy as np
 import grid as gd
+from matplotlib import pyplot
 import random
 import math
 
 def hardestDFSMaze(dimm):
     """
-    TODO: Document
+    Attempts to find the maze that causes DFS to have the largest fringe
+    @params dimm: the dimension of the desired maze
+    @return The hardest maze
     """
     bestFringe = 0
     bestMaze = []
-    for i in range(20):
+    for i in range(8): #Random restart loop. We will take the best maximum out of every restart
+        print(i, flush=True)
         maze = gd.generateSolvableGrid(dimm, .3, DFS)
         largestFringe = 0
         hardestMaze = maze
         time = 1
-        while time < 50000:
+        while time < 20000: #This is our search loop, we have this many iterations to find a local maximum
             _, path, fringeSize = DFS(maze, (0, 0), (dimm-1, dimm-1))
-            if fringeSize >= largestFringe or random.random() < math.exp(-abs(fringeSize - largestFringe) * time / 1000):
+            #This if represents taking a step into a new state in our local search
+            #We always step forward if its better, or with the simulated annealing probability function discussed in class
+            if fringeSize >= largestFringe or random.random() < math.exp(-abs(fringeSize - largestFringe) * time / 200):
                 largestFringe = fringeSize
                 hardestMaze = maze
 
             maze = mutateMaze(maze, DFS, path)
             time += 1
 
-        print(hardestMaze)
-        print(bestMaze, flush = True)
+        #This tracks the best local maximum we've found so far
         if largestFringe > bestFringe:
             bestFringe = largestFringe
             bestMaze = hardestMaze
@@ -33,23 +38,28 @@ def hardestDFSMaze(dimm):
 
 def mutateMaze(maze, algorithm, path):
     """
-    TODO: Doc
+    Changes the given maze by changing either a space to a wall or a wall to a space
+    @params maze: given maze to be mutate, algorithm: which algorithm to use to check solvability,
+        path: current path (to be used to influence mutation)
+    @return the new maze
     """
     dimm = len(maze)-1
+    #This loop keeps us from creating an unsolvable maze, it returns once it finds a solvable one
     while True:
-        newMaze = maze
+        newMaze = maze.copy()
         x = 0
         y = 0
+        #This loop attempts to find a random place in the maze to mutate that won't violate any rules
         while x == 0 and y == 0 or x == dimm and y == dimm or x < 0 or y < 0 or x > dimm or y > dimm:
-            if random.random() < 0.1:
+            if random.random() < 0.8: #Take a purely random location
                 x = random.randint(0, dimm)
                 y = random.randint(0, dimm)
-            else:
+            else: #Take a location near the current path, since a mutation there is more likely to increase maze difficulty
                 deviation = [(0, 0), (0, 1), (1, 0), (-1, 0), (0, -1)]
                 x, y = tuple(sum(x) for x in zip(random.choice(path), random.choice(deviation)))
         newMaze[y][x] = -1 if newMaze[y][x] == 0 else 0
         solved, _, _ = algorithm(newMaze, (0, 0), (dimm, dimm))
-        if solved:
+        if solved: #As long as the maze is solvable, we're good to return
             return newMaze
 
 
@@ -171,8 +181,19 @@ def makePath(start, goal, closedList):
     return path
 
 def main():
-    hardestDFSMaze(9)
     print("Testing algorithms.py")
+
+    hardestMaze = hardestDFSMaze(50)
+    _, path, fringeSize = DFS(hardestMaze, (0, 0), (49, 49))
+    for y, x in path:
+        hardestMaze[y][x] = 3
+    pyplot.matshow(hardestMaze)
+    pyplot.title(fringeSize)
+    pyplot.show()
+    for y, x in path:
+        hardestMaze[y][x] = 0
+
+
     dimm = 10
     start = (0,0)
     goal = (dimm-1,dimm-1)
